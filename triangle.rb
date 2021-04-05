83d954edd339252154fc4c25da2c5f06b85fb5bc
erikadenedu <eaden@chicagobooth.edu>
2021-04-05 18:45:07 +0000

    Completed koan

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,25 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b 
+    case1 = 1
+  else case1 = 0
+  end 
+  if b == c 
+    case2 = 1 
+  else case2 = 0
+  end 
+  if a == c  
+    case3 = 1 
+  else case3 = 0
+  end 
+  if case1 + case2 + case3 == 3 
+    return :equilateral  
+  elsif case1 + case2 + case3 == 1
+    return :isosceles 
+  else  
+    return :scalene 
+  end 
 end
 
 # Error class used in part 2.  No need to change this code.
